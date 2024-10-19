f4b0fd6a5092f0c6f732580347a27a63d1949c6f
Akash-021 <113983034+Akash-021@users.noreply.github.com>
2024-10-19 21:44:00 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const myURI = "mongodb://localhost:27017/";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || myURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
